b2a4b097ae9948e4d215bf5335b5e0581c31314c
takuto kudo <ta9to@ahunrupar.co>
2015-12-08 21:48:41 +0900

    [new]usernameを変更可能にした ログイン直後は稼働ログ画面へ飛ばす

--- config/routes.rb
+++ config/routes.rb
@@ -57,5 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'posts#index'
+  root 'action_logs#index'
 end
